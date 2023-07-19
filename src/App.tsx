e5a2fa630785e24cf70a3dd047d90db6c8f81edc
Sgnahore <stephaniergnahore@gmail.com>
2023-07-19 19:54:09 +0100

    Buttons working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import { greet } from "./utils/greet";
+import React, { useState } from "react";
+import "./App.css";
 
-function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+const COLOURS = ["pink", "green", "blue", "yellow", "purple"];
+
+function App() {
+  const [backgroundColor, setBackgroundColor] = useState(COLOURS[0]);
+
+  const onButtonClick = (colour: string) => () => {
+    setBackgroundColor(colour);
+  };
+
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor,
+      }}
+    >
+      {COLOURS.map((colour) => (
+        <button
+          type="button"
+          key={colour}
+          onClick={onButtonClick(colour)}
+          className={backgroundColor === colour ? "selected" : ""}
+        >
+          {colour}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default App;
